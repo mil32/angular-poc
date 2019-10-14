c5a119fb34b7c8715f801e1a26591651542a7bbe
Victor <Yaez>
2019-10-14 12:03:16 -0300

    startup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { AddComponent } from './add/add.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListComponent,
+    AddComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
